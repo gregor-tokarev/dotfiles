ee81e4ebb70ac0718eee39b2e82b136fbeebb974
gregor <e@tokarev.work>
2024-07-16 15:22:38 +0300

    Add `live_grep` to nvim config

--- nvim/.config/nvim/lua/gregortokarev/plugins/telescope.lua
+++ nvim/.config/nvim/lua/gregortokarev/plugins/telescope.lua
@@ -6,9 +6,10 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+		-- vim.keymap.set('n', '<leader>ps', function()
+		-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		-- end)
 
 		require("telescope").setup({
 			defaults = {
