d9c4b25b9f38bbe0866ae5d1492feb89d0541cf4
gregor <e@tokarev.work>
2024-07-18 11:09:04 +0300

    add new telescope shortcuts

--- nvim/.config/nvim/lua/gregortokarev/plugins/telescope.lua
+++ nvim/.config/nvim/lua/gregortokarev/plugins/telescope.lua
@@ -7,6 +7,8 @@ return {
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>pr', builtin.oldfiles, {})
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 		-- vim.keymap.set('n', '<leader>ps', function()
 		-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		-- end)
