540e3a3428773f96fe36ab6bf1b9cf3e1dbd6575
gregortokarev <tokarev.egor3@wildberries.work>
2025-01-31 07:14:21 +0300

    Trying to add `.proto` support

--- nvim/.config/nvim/lua/gregortokarev/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/gregortokarev/plugins/treesitter.lua
@@ -1,10 +1,21 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        opts = {
-            ensure_installed = { "lua", "markdown", "markdown_inline", "astro", "typescript", "vue", "zig", "rust", "go" },
-            auto_install = true
-        }
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"astro",
+				"typescript",
+				"vue",
+				"zig",
+				"rust",
+				"go",
+				"proto",
+			},
+			auto_install = true,
+		},
+	},
 }
