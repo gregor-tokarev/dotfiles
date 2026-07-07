3a889c0c5471d2f0beb6531606b79c2b52cbec3e
gregortokarev <e@tokarev.work>
2026-07-07 12:32:05 +0300

    fix: change rwb folder

--- zsh/.config/zsh/wbx.zsh
+++ zsh/.config/zsh/wbx.zsh
@@ -1,5 +1,5 @@
 alias tsh17='TELEPORT_HOME=${HOME}/.tsh17 TELEPORT_PROXY=tp.rwb.ru:443 TELEPORT_CLUSTER=teleport-rwb /Applications/tsh17.app/Contents/MacOS/tsh'
-alias wb='cd ~/work/wbx && spf'
+alias wb='cd ~/work && spf'
 
 export KUBECONFIG=~/.kube/k8s-wbxindex-nb.yaml
 export GOPRIVATE='gitlab.wildberries.ru/*'
